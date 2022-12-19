0bb533750776bdb99091b3fa0750128507d495fa
Rostsislav Tsirul <rost.ts@mail.ru>
2022-12-20 00:46:31 +0300

    coins component

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoinsComponent } from './coins/coins.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',component: CoinsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ front-end/src/app/coins/coins.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CoinsService } from './coins.service';
+import { Coin } from '../interfaces/coin.interface';
+
+@Component({
+  selector: 'app-coins',
+  templateUrl: './coins.component.html',
+  styleUrls: ['./coins.component.scss']
+})
+export class CoinsComponent implements OnInit{
+  coins: Coin;
+  
+  constructor(private service:CoinsService) {}
+  
+  ngOnInit() {
+      this.service.getCoins()
+        .subscribe(response => {
+          this.coins = response;         
+        });
+  }
+}
